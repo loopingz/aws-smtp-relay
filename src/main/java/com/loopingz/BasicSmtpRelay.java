9662580f276c3bf135e069e6e28c4f878ec1f1eb
Remi Cattiau <remi@cattiau.com>
2019-09-11 09:39:31 +0100

    style: fix style to match checkstyle rules

--- src/main/java/com/loopingz/BasicSmtpRelay.java
+++ src/main/java/com/loopingz/BasicSmtpRelay.java
@@ -1,62 +1,68 @@
 package com.loopingz;
 
-import com.amazonaws.util.StringUtils;
-
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import com.amazonaws.util.StringUtils;
+
 public class BasicSmtpRelay extends SmtpRelay {
 
-    private Properties props;
-    private boolean authRequest;
+  private Properties props;
+  private boolean authRequest;
 
-    BasicSmtpRelay(DeliveryDetails deliveryDetails) {
-    	super(deliveryDetails);
+  BasicSmtpRelay(DeliveryDetails deliveryDetails) {
+    super(deliveryDetails);
 
-        props = new Properties();
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", deliveryDetails.getSmtpHost());
-        props.put("mail.smtp.port", deliveryDetails.getSmtpPort());
+    props = new Properties();
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.smtp.host", deliveryDetails.getSmtpHost());
+    props.put("mail.smtp.port", deliveryDetails.getSmtpPort());
 
-        if (!StringUtils.isNullOrEmpty(deliveryDetails.getSmtpUsername()) && !StringUtils.isNullOrEmpty(deliveryDetails.getSmtpPassword())) {
-            props.put("mail.smtp.auth", "true");
-            authRequest = true;
-        } else {
-            props.put("mail.smtp.auth", "false");
-            authRequest = false;
-        }
+    if (!StringUtils.isNullOrEmpty(deliveryDetails.getSmtpUsername())
+        && !StringUtils.isNullOrEmpty(deliveryDetails.getSmtpPassword())) {
+      props.put("mail.smtp.auth", "true");
+      authRequest = true;
+    } else {
+      props.put("mail.smtp.auth", "false");
+      authRequest = false;
     }
+  }
 
-    @Override
-    public void deliver(String from, String to, InputStream inputStream) throws IOException {
+  @Override
+  public void deliver(String from, String to, InputStream inputStream) throws IOException {
 
-        try {
-            Session session = getSession();
-            Message msg = new MimeMessage(session, inputStream);
-            msg.setFrom();
-            msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
-            msg.setFrom(new InternetAddress(from));
+    try {
+      Session session = getSession();
+      Message msg = new MimeMessage(session, inputStream);
+      msg.setFrom();
+      msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+      msg.setFrom(new InternetAddress(from));
 
-            Transport.send(msg);
-        } catch (MessagingException ex) {
-            throw new IOException(ex.getMessage(), ex);
-        }
+      Transport.send(msg);
+    } catch (MessagingException ex) {
+      throw new IOException(ex.getMessage(), ex);
     }
+  }
 
-    private Session getSession() {
-        if (authRequest) {
-            return Session.getDefaultInstance(props,
-                    new Authenticator() {
-                        @Override
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(deliveryDetails.getSmtpUsername(), deliveryDetails.getSmtpPassword());
-                        }
-                    });
+  private Session getSession() {
+    if (authRequest) {
+      return Session.getDefaultInstance(props, new Authenticator() {
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication() {
+          return new PasswordAuthentication(deliveryDetails.getSmtpUsername(), deliveryDetails.getSmtpPassword());
         }
-        return Session.getDefaultInstance(props);
+      });
     }
+    return Session.getDefaultInstance(props);
+  }
 }
--- src/main/java/com/loopingz/SmtpRelay.java
+++ src/main/java/com/loopingz/SmtpRelay.java
@@ -7,24 +7,23 @@ import org.subethamail.smtp.helper.SimpleMessageListener;
 import org.subethamail.smtp.server.SMTPServer;
 
 public abstract class SmtpRelay implements SimpleMessageListener {
-    protected DeliveryDetails deliveryDetails;
-    
-    SmtpRelay(DeliveryDetails deliveryDetails) {
-    	this.deliveryDetails = deliveryDetails;
-    }
-    
-	@Override
-	public boolean accept(String from, String recipient) {
-		return true;
-	}
+  protected DeliveryDetails deliveryDetails;
 
-    void run() throws UnknownHostException {
-        SMTPServer.Builder builder = new SMTPServer.Builder();
-        builder.bindAddress(InetAddress.getByName(deliveryDetails.getBindAddress()))
-                .port(deliveryDetails.getPort())
-                .simpleMessageListener(this);
-        SMTPServer smtpServer = builder.build();
-        smtpServer.start();
-    }
+  SmtpRelay(DeliveryDetails deliveryDetails) {
+    this.deliveryDetails = deliveryDetails;
+  }
+
+  @Override
+  public boolean accept(String from, String recipient) {
+    return true;
+  }
+
+  void run() throws UnknownHostException {
+    SMTPServer.Builder builder = new SMTPServer.Builder();
+    builder.bindAddress(InetAddress.getByName(deliveryDetails.getBindAddress())).port(deliveryDetails.getPort())
+        .simpleMessageListener(this);
+    SMTPServer smtpServer = builder.build();
+    smtpServer.start();
+  }
 
 }
